6689d49da1b000887bd09f3490cb6356804e1a34
Vilhelm Hovbäck <ville.hovback@gmail.com>
2024-10-31 22:01:32 +0100

    Test
    test commit

--- app/src/main/java/com/example/projectnotes/MainActivity.kt
+++ app/src/main/java/com/example/projectnotes/MainActivity.kt
@@ -8,6 +8,10 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Delete
+import androidx.compose.material.icons.filled.Edit
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.Info
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
@@ -50,6 +54,12 @@ fun NotesApp() {
     NavHost(navController = navController, startDestination = "notesList") {
         composable("notesList") { NotesListScreen(navController, notesList) }
         composable("addListItem") { AddListItemScreen(navController, notesList) }
+        composable("detailedViewListItem/{noteId}") {
+            DetailedViewListItem(navController, it.arguments?.getString("noteId")?.toInt() ?: 0, notesList)
+        }
+        composable("editListItem/{noteId}") {
+            EditListItemScreen(navController, it.arguments?.getString("noteId")?.toInt() ?: 0, notesList)
+        }
     }
 }
 
@@ -71,23 +81,198 @@ fun NotesListScreen(navController: NavController, notesList: MutableList<NotesIt
         }
     ) { paddingValues ->
         LazyColumn(
-            modifier = Modifier.padding(paddingValues)
+            modifier = Modifier
+                .padding(paddingValues)
         ) {
             items(notesList) { note ->
-                Column(modifier = Modifier.padding(16.dp)) {
-                    Text(text = "${note.header}: ${note.description}")
+                Column(modifier = Modifier
+                    .padding(
+                        horizontal = 16.dp,
+                        //vertical = 8.dp,
+
+                    )
+
+                ) {
+                    Text( text = note.header , style = MaterialTheme.typography.headlineMedium )
                     Divider(thickness = 2.dp)
                 }
+                Row(
+                    modifier = Modifier
+                        .padding(
+                            horizontal = 16.dp,
+                            vertical = 8.dp
+                        )
+                ) {
+
+                    SmallFloatingActionButton(
+                        onClick = {
+                            notesList.remove( note )
+                        }
+                    ) {
+                            Icon(Icons.Filled.Delete, contentDescription = "Delete note")
+                        }
+
+                    SmallFloatingActionButton(
+                        onClick = {
+                            navController.navigate("detailedViewListItem/${note.id}")
+                        }
+                    ) {
+                        Icon(Icons.Filled.Info, contentDescription = "Detailed view note")
+                    }
+
+                    SmallFloatingActionButton(
+                        onClick = {
+                            navController.navigate("editListItem/${note.id}")
+                        }
+                    ) {
+                        Icon(Icons.Filled.Edit, contentDescription = "Edit view note")
+                    }
+                }
             }
+
         }
     }
 }
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
+fun EditListItemScreen(navController: NavController, noteId: Int, notesList: MutableList<NotesItem>) {
+    val note = notesList.find { it.id == noteId }
+
+    if (note == null) {
+        navController.popBackStack()
+        return
+    }
+    else {
+        val header = remember { mutableStateOf(note.header) }
+        val description = remember { mutableStateOf(note.description) }
+        var errorMsg = remember { mutableStateOf("") }
+
+        Scaffold(
+            topBar = {
+                CenterAlignedTopAppBar(
+                    title = {
+                        Text(text = "Edit Note")
+                    }
+                )
+                SmallFloatingActionButton(
+                    onClick = { navController.popBackStack() },
+                    containerColor = MaterialTheme.colorScheme.secondaryContainer,
+                    contentColor = MaterialTheme.colorScheme.secondary,
+                ) {
+                    Icon(Icons.Filled.Home, "Small floating action button.")
+                }
+            }
+        ) { paddingValues ->
+            Column(
+                modifier = Modifier
+                    .padding(paddingValues)
+                    .padding(16.dp)
+            ) {
+                OutlinedTextField(
+                    value = header.value,
+                    onValueChange = { header.value = it },
+                    label = { Text("Header") },
+                    modifier = Modifier.fillMaxWidth()
+                )
+
+                Spacer(modifier = Modifier.height(10.dp))
+
+                OutlinedTextField(
+                    value = description.value,
+                    onValueChange = { description.value = it },
+                    label = { Text("Description") },
+                    modifier = Modifier.fillMaxWidth()
+                )
+
+                Spacer(modifier = Modifier.height(16.dp))
+
+                Text (
+                    text = errorMsg.value,
+                    color = MaterialTheme.colorScheme.error
+                )
+
+                Button(
+                    onClick = {
+                        if (header.value.isNotBlank() && description.value.isNotBlank() &&
+                            header.value.length <= 20 && description.value.length <= 200 &&
+                            header.value.length >= 3 && description.value.length >= 10) {
+
+                            note.header = header.value
+                            note.description = description.value
+
+                            navController.popBackStack()
+                        } else  {
+                            // kotlin case loop; when {}
+                            when {
+                                header.value.length < 3 -> errorMsg.value = "Header must be at least 3 characters"
+                                description.value.length < 10 -> errorMsg.value = "Description must be at least 10 characters"
+                                header.value.length > 20 -> errorMsg.value = "Header must be at most 20 characters"
+                                description.value.length > 100 -> errorMsg.value = "Description must be at most 100 characters"
+                                else -> errorMsg.value = "Both header and description is needed"
+                            }
+                        }
+                    },
+                    modifier = Modifier.align(Alignment.End)
+                ) {
+                    Text("Save changes")
+                }
+            }
+        }
+
+
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun DetailedViewListItem(navController: NavController, noteId: Int, notesList: MutableList<NotesItem>) {
+
+    val note = notesList.find { it.id == noteId }
+
+    if (note == null) {
+        navController.popBackStack()
+        return
+    }
+    else {
+        Scaffold(
+            topBar = {
+                CenterAlignedTopAppBar(
+                    title = {
+                        Text(text = "Detailed view")
+                    }
+                )
+                SmallFloatingActionButton(
+                    onClick = { navController.popBackStack() },
+                    containerColor = MaterialTheme.colorScheme.secondaryContainer,
+                    contentColor = MaterialTheme.colorScheme.secondary,
+                ) {
+                    Icon(Icons.Filled.Home, "Small floating action button.")
+                }
+            }
+        ) { paddingValues ->
+            Column(
+                modifier = Modifier
+                    .padding(paddingValues)
+                    .padding(16.dp)
+                    .padding(top = 50.dp)
+            ) {
+                Spacer(modifier = Modifier.height(16.dp))
+                Text(text = "${note.header} ID: ${note.id}")
+                Text(text = note.description)
+            }
+        }
+    }
+}
+
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
 fun AddListItemScreen(navController: NavController, notesList: MutableList<NotesItem>) {
     val header = remember { mutableStateOf("") }
     val description = remember { mutableStateOf("") }
+    var errorMsg = remember { mutableStateOf("") }
+
 
     Scaffold(
         topBar = {
@@ -96,6 +281,13 @@ fun AddListItemScreen(navController: NavController, notesList: MutableList<Notes
                     Text(text = "Add Note")
                 }
             )
+            SmallFloatingActionButton(
+                onClick = { navController.popBackStack() },
+                containerColor = MaterialTheme.colorScheme.secondaryContainer,
+                contentColor = MaterialTheme.colorScheme.secondary,
+            ) {
+                Icon(Icons.Filled.Home, "Small floating action button.")
+            }
         }
     ) { paddingValues ->
         Column(
@@ -110,7 +302,7 @@ fun AddListItemScreen(navController: NavController, notesList: MutableList<Notes
                 modifier = Modifier.fillMaxWidth()
             )
 
-            Spacer(modifier = Modifier.height(8.dp))
+            Spacer(modifier = Modifier.height(10.dp))
 
             OutlinedTextField(
                 value = description.value,
@@ -121,9 +313,17 @@ fun AddListItemScreen(navController: NavController, notesList: MutableList<Notes
 
             Spacer(modifier = Modifier.height(16.dp))
 
+            Text (
+                text = errorMsg.value,
+                color = MaterialTheme.colorScheme.error
+            )
+
             Button(
                 onClick = {
-                    if (header.value.isNotBlank() && description.value.isNotBlank()) {
+                    if (header.value.isNotBlank() && description.value.isNotBlank() &&
+                        header.value.length <= 20 && description.value.length <= 200 &&
+                        header.value.length >= 3 && description.value.length >= 10) {
+
                         val newNote = NotesItem(
                             id = (notesList.size + 1),
                             header = header.value,
@@ -135,8 +335,15 @@ fun AddListItemScreen(navController: NavController, notesList: MutableList<Notes
                         description.value = ""
 
                         navController.popBackStack()
-                    } else {
-                        // Handle empty fields
+                    } else  {
+                        // kotlin case loop; when {}
+                        when {
+                            header.value.length < 3 -> errorMsg.value = "Header must be at least 3 characters"
+                            description.value.length < 10 -> errorMsg.value = "Description must be at least 10 characters"
+                            header.value.length > 20 -> errorMsg.value = "Header must be at most 20 characters"
+                            description.value.length > 100 -> errorMsg.value = "Description must be at most 100 characters"
+                            else -> errorMsg.value = "Both header and description is needed"
+                        }
                     }
                 },
                 modifier = Modifier.align(Alignment.End)
